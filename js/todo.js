3f7c628832790891b832ab60fe13874ec40dc58c
krystek77 <krystian.wrona77@gmail.com>
2019-11-12 08:49:22 +0100

    create constructor task

---
+++ js/todo.js
@@ -0,0 +1,23 @@
+let id = 0;
+
+/**
+ * Create Tsk object
+ *
+ * @param {String} content
+ * @param {boolean} done
+ * @param {boolean} trash
+ */
+
+function Task(content, done = false, trash = false) {
+	id = id + 1;
+	this.id = id;
+	this.content = content;
+	this.done = done;
+	this.trash = trash;
+}
+
+Task.prototype.toString = function() {
+	console.log(`Task: ID=${this.id};content=${this.content};done=${this.done};trash=${this.trash}`);
+};
+
+
