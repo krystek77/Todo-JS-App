253646389a27df414668b244248c5d202059f7fb
krystek77 <krystian.wrona77@gmail.com>
2019-11-12 09:27:46 +0100

    add function to add task to the list of tasks

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,6 @@
 let id = 0;
+const taskList = document.querySelector('.todo-application--task-lists');
+const addIcon = document.querySelector('.icon__add');
 
 /**
  * Create Tsk object
@@ -24,3 +26,29 @@ Task.prototype.toString = function() {
 		trash=${this.trash}
 		`);
 };
+/**
+ * Adds task to list of tasks after last child
+ *
+ * @param {Task} task
+ */
+function addTask(task) {
+	const done = task.done;
+	const trash = task.trash;
+	const content = task.content;
+	const id = task.id;
+
+	const CHECK = 'fas fa-check-circle';
+	const UNCHECK = 'fa fa-circle';
+	const DONE = done ? CHECK : UNCHECK;
+
+	const position = 'beforeend';
+
+	const element = `
+		<li class="todo-application--task">
+			<i class="fa ${DONE} icon icon__done todo-application--icon" id="${id}"></i>
+			<p class="todo-application--content">${content}</p>
+			<i class="fa fa-trash-alt icon icon__trash todo-application--icon" id="${id}"></i>
+		</li>`;
+
+	taskList.insertAdjacentHTML(position, element);
+}
